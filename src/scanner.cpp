8d7f72d8ab3e403190e45fedce3dd80633df733a
Winston Riggs <wriggs12@gmail.com>
2023-07-26 20:36:03 -0400

    Open Camera

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -1,6 +1,23 @@
 #include "../include/scanner.h"
+#include <iostream>
 
 int main()
 {
+    cv::VideoCapture cap(0);
+
+    if (!cap.isOpened())
+    {
+        std::cout << "Use a different camera port number!" << std::endl;
+        return -1;
+    }
+
+    while (true)
+    {
+        cv::Mat frame;
+        cap.read(frame);
+        imshow("camera", frame);
+        cv::waitKey(1);
+    }
+
     return 0;
 }
